ba68289b4ce38a3e024ef67f95bd0a544d200401
Serch-conkersoup <ius.iurerodolfo@gmail.com>
2025-11-22 23:00:07 -0600

    Update ejercicio03.js

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,6 +1,9 @@
 // Ejercicio 3: Suma de Dos Números
 // Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
 
+function sumar(a, b) {
+  return a + b;
+}
 //Escribe tu función aquí
 
 
